f7a8024af02410da83b6f47647fff9736c1edac0
VladimirSapronov <60099043+VladimirSapronov@users.noreply.github.com>
2020-06-05 17:56:30 +0300

    Исправил ошибки npm test

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var WIZARD_NAMES = ['Иван','Хуан Себастьян','Мария','Кристоф','Виктор','Юлия','Люпита','Вашингтон'];
-var WIZARD_SURNAME = ['да Марья','Верон','Мирабелла','Вальц','Онопко','Топольницка','Нионго','Ирвинг'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницка', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
 var maxlength = [WIZARD_NAMES, WIZARD_SURNAME, WIZARD_COAT, WIZARD_EYES];
@@ -43,9 +43,9 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-var getRandomInt = function(max) {
+var getRandomInt = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
-}
+};
 
 var createWizards = function (wizard) {
   for (var j = 0; j < maxlength.length; j++) {
@@ -55,9 +55,9 @@ var createWizards = function (wizard) {
   wizard.surname = WIZARD_SURNAME[maxIndex[1]];
   wizard.coatColor = WIZARD_COAT[maxIndex[2]];
   wizard.eyesColor = WIZARD_EYES[maxIndex[3]];
-}
+};
 
-var renderWizard = function(wizard) {
+var renderWizard = function (wizard) {
   createWizards(wizard);
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -66,7 +66,7 @@ var renderWizard = function(wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
