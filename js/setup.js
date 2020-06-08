77a2e4052e42397f9840094e8fb528ab897b5e73
VladimirSapronov <60099043+VladimirSapronov@users.noreply.github.com>
2020-06-08 17:30:56 +0300

    Отредактировал код согласно полученным замечаниям

--- js/setup.js
+++ js/setup.js
@@ -1,36 +1,34 @@
 'use strict';
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницка', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-var maxlength = [WIZARD_NAMES, WIZARD_SURNAME, WIZARD_COAT, WIZARD_EYES];
-var maxIndex = ['names', 'surname', 'coat', 'eyes'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницка', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var wizards = [
   {
     name: WIZARD_NAMES,
-    surname: WIZARD_SURNAME,
-    coatColor: WIZARD_COAT,
-    eyesColor: WIZARD_EYES
+    surname: WIZARD_SURNAMES,
+    coatColor: WIZARD_COAT_COLORS,
+    eyesColor: WIZARD_EYES_COLORS
   },
   {
     name: WIZARD_NAMES,
-    surname: WIZARD_SURNAME,
-    coatColor: WIZARD_COAT,
-    eyesColor: WIZARD_EYES
+    surname: WIZARD_SURNAMES,
+    coatColor: WIZARD_COAT_COLORS,
+    eyesColor: WIZARD_EYES_COLORS
   },
   {
     name: WIZARD_NAMES,
-    surname: WIZARD_SURNAME,
-    coatColor: WIZARD_COAT,
-    eyesColor: WIZARD_EYES
+    surname: WIZARD_SURNAMES,
+    coatColor: WIZARD_COAT_COLORS,
+    eyesColor: WIZARD_EYES_COLORS
   },
   {
     name: WIZARD_NAMES,
-    surname: WIZARD_SURNAME,
-    coatColor: WIZARD_COAT,
-    eyesColor: WIZARD_EYES
+    surname: WIZARD_SURNAMES,
+    coatColor: WIZARD_COAT_COLORS,
+    eyesColor: WIZARD_EYES_COLORS
   }
 ];
 
@@ -43,18 +41,16 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-var getRandomInt = function (max) {
-  return Math.floor(Math.random() * Math.floor(max));
+var getRandomElement = function (wizardАttribute) {
+  var indexElement = Math.floor(Math.random() * Math.floor(wizardАttribute.length));
+  return wizardАttribute[indexElement];
 };
 
 var createWizards = function (wizard) {
-  for (var j = 0; j < maxlength.length; j++) {
-    maxIndex[j] = getRandomInt(maxlength[j].length);
-  }
-  wizard.name = WIZARD_NAMES[maxIndex[0]];
-  wizard.surname = WIZARD_SURNAME[maxIndex[1]];
-  wizard.coatColor = WIZARD_COAT[maxIndex[2]];
-  wizard.eyesColor = WIZARD_EYES[maxIndex[3]];
+  wizard.name = getRandomElement(WIZARD_NAMES);
+  wizard.surname = getRandomElement(WIZARD_SURNAMES);
+  wizard.coatColor = getRandomElement(WIZARD_COAT_COLORS);
+  wizard.eyesColor = getRandomElement(WIZARD_EYES_COLORS);
 };
 
 var renderWizard = function (wizard) {
