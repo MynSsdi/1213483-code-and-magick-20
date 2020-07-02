ae2a7272ab9ebd401b7d8ba754e91747a2a6b8b4
VladimirSapronov <60099043+VladimirSapronov@users.noreply.github.com>
2020-07-02 17:28:09 +0300

    Добавил перемещение диалогового окна

--- js/dialog.js
+++ js/dialog.js
@@ -6,6 +6,7 @@
   var setupWizardForm = document.querySelector('.setup-wizard-form');
   var openUserDialog = document.querySelector('.setup-open');
   var closeUserDialog = document.querySelector('.setup-close');
+  var dialogHandle = document.querySelector('.upload');
 
   var onPopupEscPress = function (evt) {
     window.util.isEscEvent(evt, closePopup);
@@ -41,4 +42,55 @@
     window.util.isEnterEvent(evt, closePopup);
   });
 
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
+
 })();
+
+document.removeEventListener('click', function (evt) {
+  evt.preventDefault();
+});
